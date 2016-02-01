0d7059887c24ba26e1258772242f0ab332c6350c
Kaveh Vakili <kaveh.vakili@gmail.com>
2016-02-01 01:30:23 -0500

    Added the buy ticket functionality

--- src/app/app.controller.js
+++ src/app/app.controller.js
@@ -3,16 +3,76 @@
 	/**
 	 * This is the Lottery App Controller.
 	 */
-	module.controller('LotteryController', function() {
+	module.controller('LotteryController', function(Ball, $mdDialog) {
+		var TICKET_PRICE = 10;
+
 		var self = this;
-		self.getPotAmount = getPotAmount;
+		self.buyNewTicket = buyNewTicket;
+		self.initialize = initialize;
+		self.openBuyTicket = openBuyTicket;
 
 		//Amount of money in Pot
-		var pot = 200;
+		self.potAmount = 200;
 
-		function getPotAmount() {
-			return pot;
+		self.lotteryBalls = [];
+		self.unassignedBalls = [];
+		self.assignedBalls = [];
+		self.cachedTickets = [];
+
+		self.initialize();
+
+		function initialize() {
+			for (var i = 1; i <= 50; i++) {
+				var ball = new Ball(i);
+				self.lotteryBalls.push(ball);
+				if (self.cachedTickets.length) {
+					var ticket = self.cachedTickets.pop();
+					payTheTicket(ball, ticket.name, ticket.price);
+				} else {
+					self.unassignedBalls.push(ball);
+				}
+			}
+		}
+
+		function buyNewTicket(name) {
+			if (self.unassignedBalls.length) {
+				var ball = self.unassignedBalls.pop();
+				payTheTicket(ball, name, TICKET_PRICE);
+			} else {
+				self.cachedTickets.push({name: name, price: TICKET_PRICE});
+			}
+		}
+
+		function payTheTicket(ball, name, ticketPrice) {
+			ball.assignName(name);
+			self.assignedBalls.push(name);
+			self.potAmount = self.potAmount + ticketPrice;
+		}
+
+		function openBuyTicket(ev) {
+			$mdDialog.show({
+				controller: DialogController,
+				templateUrl: 'app/dialogs/buy-ticket.html',
+				parent: angular.element(document.body),
+				targetEvent: ev,
+				clickOutsideToClose: true
+			}).then(function(results) {
+				console.log(results);
+				self.buyNewTicket(self.newBuyer);
+			});
 		}
 	});
-
 })(angular.module('lottery'));
+
+function DialogController($scope, $mdDialog) {
+	'use strict';
+
+  $scope.ok = function() {
+		if (!$scope.newBuyerForm.$invalid) {
+			$mdDialog.hide($scope.newBuyer);
+		}
+  };
+  $scope.cancel = function() {
+    $mdDialog.cancel();
+  };
+}
---
+++ src/app/domain/ball.js
@@ -0,0 +1,29 @@
+(function(module) {
+  'use strict';
+
+  function BallFactory() {
+    /**
+     * @ngdoc type
+     * @name Ball
+     * @kind class
+     * @module lottery
+     *
+     * @description
+     * represents a Ball that will be drawn in the lottery
+     *
+     * @property {number} number, example: 10
+     * @property {string} name, example: 'Kaveh'
+     */
+    function Ball(number) {
+      this.number = number;
+    }
+
+    Ball.prototype.assignName = function(name) {
+      this.name = name;
+    };
+
+    return Ball;
+  }
+
+  module.factory('Ball', BallFactory);
+})(angular.module('lottery'));
